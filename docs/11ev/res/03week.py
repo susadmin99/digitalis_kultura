d60416a1c176c95e344cca93fdca501a0526ddcc
András Joó <joo.andras@protonmail.com>
2022-09-19 08:29:08 +0200

    03week res added to class 11

---
+++ docs/11ev/res/03week.py
@@ -0,0 +1,14 @@
+ablak = 10
+print(ablak)
+ajto = int(input("Ajtó mérete: "))
+print(str(ablak) + str(ajto))
+
+if ajto == 20:
+    print("Az ajtó az 20.")
+    print(ablak*ajto)
+elif ajto == 30:
+    print("Az ajtó az 30.")
+    print(ablak/ajto)
+
+print(ajto == 30)
+
