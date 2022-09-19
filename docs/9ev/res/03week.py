d4c3ee10d17bfbf8940a1ca04b45e1c2915ef28c
András Joó <joo.andras@protonmail.com>
2022-09-19 10:51:23 +0200

    03week class exercise added to 10 and 9 classes

---
+++ docs/9ev/res/03week.py
@@ -0,0 +1,17 @@
+
+ablak = 10
+print(ablak)
+
+ajto = int(input("Ajtó mérete: "))
+print(ablak + ajto)
+
+if ajto == 20:
+    print("Az ajtó az 20.")
+    print(ablak*ajto)
+elif ajto == 30:
+    print("Az ajtó az 30.")
+    print(ablak/ajto)
+else:
+    print(f"Az ajtó: {ajto}")
+
+print(ajto == 30)
