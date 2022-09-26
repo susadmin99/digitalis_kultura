4fcdfeb073da43ee73907d2da17589c8204507c7
András Joó <joo.andras@protonmail.com>
2022-09-26 08:36:44 +0200

    04week theory and practice added

---
+++ docs/11ev/res/04week/ora.py
@@ -0,0 +1,23 @@
+for alma in "Alma":
+    print(alma)
+
+for korte in range(10):
+    print(korte)
+
+for eper in range(10,20):
+    print(eper)
+
+for repa in range(10,50,2):
+    print(repa)
+else:
+    print("Vége")
+
+
+
+x = 10
+while x < 20:
+    print(x)
+    x+=1
+else:
+    print("Vége")
+    
