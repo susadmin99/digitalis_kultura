d5923835647cc1017466cdfea9390032c80a30c1
András Joó <joo.andras@protonmail.com>
2022-11-03 07:39:13 +0100

    9 exam uploaded, 11 lesson added

---
+++ docs/11ev/res/09week/ora.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+
+#window = tk.Tk()
+
+#hello = tk.Label(text="Hello, Tkinter")
+
+#hello.pack()
+
+#window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#szoveg1 = tk.Label(
+#    text="Hello, Tkinter",
+#    fg="white",
+#    bg="black",
+#    width=10,
+#    height=10
+#)
+
+#gomb = tk.Button(
+#    text="Click me!",
+#    width=25,
+#    height=5,
+#    bg="blue",
+#    fg="yellow",
+#)
+
+#bemenet_szoveg = tk.Label(text="Name")
+#bemenet = tk.Entry()
+
+#nev = bemenet.get()
+#print(nev)
+
+#szoveg_doboz = tk.Text()
+#szoveg_doboz.pack()
+
+#bemenet_szoveg.pack()
+#bemenet.pack()
+#gomb.pack()
+#szoveg1.pack()
+#hello.pack()
+
+#window.mainloop()
