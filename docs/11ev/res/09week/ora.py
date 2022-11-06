00f2f92e9d5c02a4191536475d2f344ba7ce3f47
susadmin99 <joo.andras.ivan01@gmail.com>
2022-11-06 21:55:47 +0100

    10 week exercise added

--- docs/11ev/res/09week/ora.py
+++ docs/11ev/res/09week/ora.py
@@ -1,55 +1,48 @@
+#Ez elég fontos dolog, enélkül nem működik semmi
 import tkinter as tk
 
-#window = tk.Tk()
+#Kell egy window vagy valami ami egyenlő a TK()-val, hogy lássunk is valamit
+window = tk.Tk()
 
-#hello = tk.Label(text="Hello, Tkinter")
+hello = tk.Label(text="Hello, Tkinter")
 
-#hello.pack()
+hello.pack()
 
-#window.mainloop()
+#Label - Egyszerű szöveg widget
+szoveg1 = tk.Label(
+    text="Hello, Tkinter",
+    fg="white",
+    bg="black",
+    width=10,
+    height=10
+)
 
+#Button - Gomb widget
+gomb = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
 
+bemenet_szoveg = tk.Label(text="Name")
 
+#Entry - Bemeneti widget
+bemenet = tk.Entry()
 
+nev = bemenet.get()
+print(nev)
 
+szoveg_doboz = tk.Text()
 
+#mindent is csomagolunk
+szoveg_doboz.pack()
+bemenet_szoveg.pack()
+bemenet.pack()
+gomb.pack()
+szoveg1.pack()
+hello.pack()
 
-
-
-
-
-
-
-
-#szoveg1 = tk.Label(
-#    text="Hello, Tkinter",
-#    fg="white",
-#    bg="black",
-#    width=10,
-#    height=10
-#)
-
-#gomb = tk.Button(
-#    text="Click me!",
-#    width=25,
-#    height=5,
-#    bg="blue",
-#    fg="yellow",
-#)
-
-#bemenet_szoveg = tk.Label(text="Name")
-#bemenet = tk.Entry()
-
-#nev = bemenet.get()
-#print(nev)
-
-#szoveg_doboz = tk.Text()
-#szoveg_doboz.pack()
-
-#bemenet_szoveg.pack()
-#bemenet.pack()
-#gomb.pack()
-#szoveg1.pack()
-#hello.pack()
-
-#window.mainloop()
+#a mainloop mindig a legutolsó sor legyen a fájlban
+window.mainloop()
---
+++ docs/11ev/res/10week/ora.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+def say_hello():
+    label2.config(text="Hello")
+
+
+def say_my_name():
+    label2.config(text=f"Hello {entry1.get()}")
+
+
+
+btn1 = tk.Button(text="Say hello", command=say_hello)
+
+label1 = tk.Label(text="Name please:")
+entry1 = tk.Entry()
+btn2 = tk.Button(text="Say my name", command=say_my_name)
+label2 = tk.Label()
+
+btn1.pack()
+label1.pack()
+entry1.pack()
+btn2.pack()
+label2.pack()
+
+
+window.mainloop()
+
+#feladat
+login_status = "You are not logged in!"
+def login():
+    if username_entry.get() == "admin" and password_entry.get() == "admin123":
+        login_status = "Successful login!"
+        login_label.config(text=login_status)
+    else:
+        login_status = "Wrong credentials! Try again!"
+        login_label.config(text=login_status)
+
+
+fl = tk.Tk()
+fl.geometry("300x150")
+
+username_label = tk.Label(text="Username")
+username_entry = tk.Entry()
+
+password_label = tk.Label(text="Password")
+password_entry = tk.Entry(show="*")
+
+login_btn = tk.Button(text="Login", command=login)
+login_label = tk.Label(text=login_status)
+
+username_label.pack()
+username_entry.pack()
+password_label.pack()
+password_entry.pack()
+login_btn.pack()
+login_label.pack()
+
+
+fl.mainloop()
+
+
+
+
