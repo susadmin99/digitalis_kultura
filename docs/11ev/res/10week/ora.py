86ff7a13154146d41bd0cba243b1835a38daa6c6
András Joó <joo.andras@protonmail.com>
2022-11-14 12:11:16 +0100

    11weekk added to 9,10,11 + kritériumok

--- docs/11ev/res/10week/ora.py
+++ docs/11ev/res/10week/ora.py
@@ -144,6 +144,7 @@ root = Tk()
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+
 canvas = Canvas(root)
 canvas.grid(column=0, row=0, sticky=(N, W, E, S))
 canvas.bind("<Button-1>", savePosn)
